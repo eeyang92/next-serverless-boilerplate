d77c9130b7383eb71a1ad6a6b58634175bbd54dc
eric.yang <eeyang92@gmail.com>
2018-12-19 02:27:25 -0800

    formatting

--- components/meta.tsx
+++ components/meta.tsx
@@ -3,8 +3,8 @@ import Head from 'next/head'
 export default ({ title, description }) => (
     <div>
         <Head>
-            <title>{title || 'Next.js Test Title'}</title>
-            <meta name='description' content={description || 'Next.js Test Description'} />
+            <title>{ title || 'Next.js Test Title' }</title>
+            <meta name='description' content={ description || 'Next.js Test Description' } />
             <meta name='viewport' content='width=device-width, initial-scale=1' />
             <meta charSet='utf-8' />
         </Head>
